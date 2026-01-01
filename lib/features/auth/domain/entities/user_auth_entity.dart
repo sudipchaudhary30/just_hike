cd4ce7de96e456a4fbb9aa4e33e46511aecf2db4
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-01 23:24:20 +0545

    Update the password, profilepicture in entities

--- lib/features/auth/domain/entities/user_auth_entity.dart
+++ lib/features/auth/domain/entities/user_auth_entity.dart
@@ -4,7 +4,9 @@ class UserAuthEntity extends Equatable {
   final String? userAuthId;
   final String fullName;
   final String email;
- 
+  final String? password;
+  final String? profilePicture;
+  final String? phoneNumber;
 
 
   const UserAuthEntity({
