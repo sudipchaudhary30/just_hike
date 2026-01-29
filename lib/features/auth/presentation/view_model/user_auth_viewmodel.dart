867f1743f02ce786cb6fceafd142078194e513f4
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-30 01:02:45 +0545

    feat: Add updateProfile method to AuthViewmodel

--- lib/features/auth/presentation/view_model/user_auth_viewmodel.dart
+++ lib/features/auth/presentation/view_model/user_auth_viewmodel.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_hike/features/auth/domain/usecases/update_profile_usecase.dart';
 import 'package:just_hike/features/auth/domain/usecases/user_login_usecase.dart';
 import 'package:just_hike/features/auth/domain/usecases/user_register_usecase.dart';
 import 'package:just_hike/features/auth/presentation/state/user_auth_state.dart';
@@ -10,11 +12,13 @@ final authViewmodelProvider = NotifierProvider<AuthViewmodel, AuthState>(
 class AuthViewmodel extends Notifier<AuthState> {
   late final RegisterUsecase _registerUsecase;
   late final LoginUsecase _loginUsecase;
+  late final UpdateProfileUsecase _updateProfileUsecase;
 
   @override
   AuthState build() {
     _registerUsecase = ref.read(registerUsecaseProvider);
     _loginUsecase = ref.read(loginUsecaseProvider);
+    _updateProfileUsecase = ref.read(updateProfileUsecaseProvider);
     return AuthState();
   }
 
@@ -51,6 +55,37 @@ class AuthViewmodel extends Notifier<AuthState> {
     final params = LoginUsecaseParams(email: email, password: password);
     await Future.delayed(const Duration(seconds: 2));
     final result = await _loginUsecase(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
+      },
+    );
+  }
+
+  // Update profile
+  Future<void> updateProfile({
+    required String fullName,
+    required String email,
+    required String phoneNumber,
+    File? profileImage,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = UpdateProfileUsecaseParams(
+      fullName: fullName,
+      email: email,
+      phoneNumber: phoneNumber,
+      profileImage: profileImage,
+    );
+    final result = await _updateProfileUsecase(params);
     result.fold(
       (failure) {
         state = state.copyWith(
