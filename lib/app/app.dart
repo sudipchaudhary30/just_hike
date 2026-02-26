0f9cc97ac3c3f1f721984b2e3bbdf6ad4e12f654
Sudip Chaudhary <chsudip30@gmail.com>
2026-02-26 23:31:47 +0545

    Add and update Riverpod providers for booking, profile, and guides

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:just_hike/features/splash/presentation/pages/splash_screen.dart';
+import 'package:just_hike/features/dashboard/presentation/screens/bottom_screen/my_trips_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashScreen(), 
-    debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      routes: {'/myTrips': (context) => MyTripsScreen()},
+    );
   }
 }
